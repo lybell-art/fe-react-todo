2ea29f618f0a0de7bc6b0ae04a595869e9d1ad5e
lybell-art <futireflugel@gmail.com>
2024-07-16 18:07:58 +0900

    [Feat] 아이템 추가 폼에 디바운스 추가

--- src/TodoComponent/TodoList/TodoListItem/index.jsx
+++ src/TodoComponent/TodoList/TodoListItem/index.jsx
@@ -1,4 +1,20 @@
+import { useState, useEffect, useCallback } from "react";
+import debounce from "@/common/debounce.js";
+
 export default function TodoListItem({data, deleteData, checkData, modifyData})
 {
-	return <li>{data.title}</li>
+	const [title, setTitle] = useState(data.title);
+	const syncData = useCallback( debounce( (data)=>{
+		modifyData(data.id, data.title);
+	}, 500 ), [] );
+	function onChange(e)
+	{
+		setTitle(e.target.value);
+		syncData(data);
+	}
+
+	return <li>
+		<input type="text" value={title} onChange={onChange} />
+		<button>삭제</button>
+	</li>
 }
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { resolve } from "path";
+
+console.log(__dirname, "src");
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "./",
   plugins: [react()],
+  resolve: {
+    alias: [
+      { find: "@", replacement: resolve(__dirname, "src") },
+    ]
+  },
 });
