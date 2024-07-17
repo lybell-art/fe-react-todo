559219f769d91acf13db5961159195ebce72385c
lybell-art <futireflugel@gmail.com>
2024-07-17 13:46:19 +0900

    [Style] 드롭 영역 스타일 추가

--- src/TodoComponent/TodoList/TodoListDropArea/index.jsx
+++ src/TodoComponent/TodoList/TodoListDropArea/index.jsx
@@ -1,16 +1,27 @@
+import { useState } from "react";
+import style from "./style.module.scss";
+
 export default function TodoListDropArea({id, moveData})
 {
+	const [isOver, setIsOver] = useState(false);
 	function onDragOver(e)
 	{
 		e.preventDefault();
+		setIsOver(true);
 	}
 	function onDrop(e)
 	{
 		e.preventDefault();
 		const dragTarget = e.dataTransfer.getData("text");
-		//console.log(e.dataTransfer.getData("text"));
 		moveData(dragTarget, id);
+		setIsOver(false);
+	}
+	function onDragLeave()
+	{
+		setIsOver(false);
 	}
 
-	return <div onDragOver={onDragOver} onDrop={onDrop}>----(개발중임)----</div>
+	return <div className={`${style.dropArea} ${isOver ? style.active : ""}`}
+		onDragOver={onDragOver} onDrop={onDrop} onDragLeave={onDragLeave}
+	></div>
 }
