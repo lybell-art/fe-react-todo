b7f3f15faeb57b36727e8d2703f149a68a8d1c80
lybell-art <futireflugel@gmail.com>
2024-07-17 13:23:48 +0900

    [Feat] 요소 드래그 기능 구현

--- src/TodoComponent/TodoList/TodoListDropArea/index.jsx
+++ src/TodoComponent/TodoList/TodoListDropArea/index.jsx
@@ -1,4 +1,14 @@
 export default function TodoListDropArea({id, moveData})
 {
-	return <div>----(개발중임)----</div>
+	function onDragOver(e)
+	{
+		e.preventDefault();
+	}
+	function onDrop(e)
+	{
+		e.preventDefault();
+		console.log(e.dataTransfer.getData("text"));
+	}
+
+	return <div onDragOver={onDragOver} onDrop={onDrop}>----(개발중임)----</div>
 }
--- src/TodoComponent/TodoList/TodoListItem/index.jsx
+++ src/TodoComponent/TodoList/TodoListItem/index.jsx
@@ -1,9 +1,11 @@
-import { useState, useEffect, useCallback } from "react";
+import { useState, useCallback } from "react";
+import useDrag from "./useDrag.js";
 import debounce from "@/common/debounce.js";
 import style from "./style.module.scss";
 
 export default function TodoListItem({data, deleteData, checkData, modifyData})
 {
+	const {style: dragStyle, onDragStart: _onDragStart, onDragging, onDragEnd} = useDrag();
 	const [title, setTitle] = useState(data.title);
 	const syncData = useCallback( debounce( (data)=>{
 		modifyData(data.id, data.title);
@@ -13,8 +15,22 @@ export default function TodoListItem({data, deleteData, checkData, modifyData})
 		setTitle(e.target.value);
 		syncData(data);
 	}
+	function onDragStart(e)
+	{
+		e.dataTransfer.clearData();
+		e.dataTransfer.setData("text/plain", data.id);
+		_onDragStart(e);
+	}
 
-	return <li className={`${style.container} ${data.completed ? style.completed : ""}`} draggable="true">
+	return <li
+		className={`${style.container} ${data.completed ? style.completed : ""}`}
+		style={dragStyle}
+		draggable="true"
+		onDragStart={onDragStart}
+		onDrag={onDragging}
+		onDragEnd={onDragEnd}
+		onDragLeave={onDragEnd}
+	>
 		<input className={style.title} type="text" value={title} onChange={onChange} readOnly={true} />
 		<button className={style.button}>삭제</button>
 	</li>
