ed7932eec46bdf22c59dd6d86e4276201942b7dc
lybell-art <futireflugel@gmail.com>
2024-07-17 14:41:42 +0900

    [Fix] 최근 검색어 보이기를 요소 포커싱으로 변경

--- src/TodoComponent/TodoSearchBar/index.jsx
+++ src/TodoComponent/TodoSearchBar/index.jsx
@@ -23,12 +23,11 @@ export default function TodoSearchBar({addData})
 		addData(inputData);
 	}
 
-	return <div
-		className={style.container}
-		onMouseEnter={()=>setRecentSearchVisiblity(true)} 
-		onMouseLeave={()=>setRecentSearchVisiblity(false)}
-	>
-		<input className={style.input} type="text" placeholder="할일을 입력하세요." ref={inputRef}/>
+	return <div className={style.container}>
+		<input className={style.input} type="text" placeholder="할일을 입력하세요." ref={inputRef}
+			onFocus = {()=>setRecentSearchVisiblity(true)}
+			onBlur={()=>setTimeout(()=>setRecentSearchVisiblity(false), 100)}
+		/>
 		<button className={style.submitButton} type="button" onClick={onClick}>등록</button>
 		<ul className={`${style.recentList} ${recentSearchVisiblity ? "" : style.hidden}`}>
 			{recentSearchState.length > 0 && recentSearchState.map( ({keyword, id})=>{
